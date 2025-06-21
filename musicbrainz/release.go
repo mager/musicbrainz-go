5d0b80c24176f55006a799e127bc5e2ec9432176
mager <andrew.mager@gmail.com>
2025-06-21 07:10:14 -0500

    Release: Artist credit plural

--- musicbrainz/release.go
+++ musicbrainz/release.go
@@ -1,12 +1,12 @@
 package musicbrainz
 
 type Release struct {
-	ID             string        `json:"id"`
-	Title          string        `json:"title"`
-	Date           string        `json:"date"`
-	Country        string        `json:"country"`
-	Disambiguation string        `json:"disambiguation"`
-	Status         string        `json:"status"`
-	Packaging      string        `json:"packaging"`
-	ArtistCredit   *ArtistCredit `json:"artist-credit"`
+	ID             string          `json:"id"`
+	Title          string          `json:"title"`
+	Date           string          `json:"date"`
+	Country        string          `json:"country"`
+	Disambiguation string          `json:"disambiguation"`
+	Status         string          `json:"status"`
+	Packaging      string          `json:"packaging"`
+	ArtistCredit   *[]ArtistCredit `json:"artist-credit"`
 }
